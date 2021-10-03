6a21cd20534179b30153e380bb95b2517e121d39
Daniel Zarifpour <daniel@zarif.pro>
2021-10-03 13:10:51 -0400

    Binding to Attributes

--- hello-world/src/App.vue
+++ hello-world/src/App.vue
@@ -2,6 +2,8 @@
   <div>{{ greet }} {{ name }}</div>
   <div v-html="username"></div>
   <div v-html="hack"></div>
+  <h2 v-bind:id="headingId">Heading</h2>
+  <button v-bind:disabled="isDisabled">Bind</button>
 </template>
 
 <script>
@@ -13,6 +15,8 @@ export default {
       name: "Daniel",
       username: "<b>zarif</b>",
       hack: `<a href="#" onclick="alert('You have been hacked!')">Win a prize!</a>`,
+      headingId: "heading",
+      isDisabled: false,
     };
   },
 };
