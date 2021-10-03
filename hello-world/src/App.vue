d1eb521315429e1890e00caf19367b0edbdb1f85
Daniel Zarifpour <daniel@zarif.pro>
2021-10-03 12:45:32 -0400

    Binding text

--- hello-world/src/App.vue
+++ hello-world/src/App.vue
@@ -1,17 +1,16 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>Hello {{ name }}</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  data() {
+    return {
+      name: "Daniel",
+    };
+  },
+};
 </script>
 
 <style>
