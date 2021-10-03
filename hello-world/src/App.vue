8b684331f6700b129b1b19c3660cc4d20ed197dd
Daniel Zarifpour <daniel@zarif.pro>
2021-10-03 14:07:19 -0400

    Binding classes conditionally

--- hello-world/src/App.vue
+++ hello-world/src/App.vue
@@ -4,6 +4,23 @@
   <div v-html="hack"></div>
   <h2 v-bind:id="headingId">Heading</h2>
   <button v-bind:disabled="isDisabled">Bind</button>
+  <h2 class="underline">Underlined text</h2>
+  <h2 class="underline" v-bind:class="status">Status</h2>
+  <h2 v-bind:class="isPromoted && 'promoted'">Promoted Movie</h2>
+  <h2 v-bind:class="isSoldout ? 'sold-out' : 'new'">Sold Out? Movie</h2>
+  <h2 v-bind:class="['new', 'promoted']">Newly Promoted Movie</h2>
+  <h2 v-bind:class="[isPromoted && 'promoted', isSoldout ? 'sold-out' : 'new']">
+    Array Conditional Movie
+  </h2>
+  <h2
+    v-bind:class="{
+      promoted: isPromoted,
+      new: !isSoldout,
+      'sold-out': isSoldout,
+    }"
+  >
+    Object Conditional Movie
+  </h2>
 </template>
 
 <script>
@@ -17,6 +34,9 @@ export default {
       hack: `<a href="#" onclick="alert('You have been hacked!')">Win a prize!</a>`,
       headingId: "heading",
       isDisabled: false,
+      status: "success",
+      isPromoted: true,
+      isSoldout: true,
     };
   },
 };
@@ -30,5 +50,21 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.underline {
+  text-decoration: underline;
+}
+
+.promoted {
+  font-style: italic;
+}
+
+.new {
+  color: olivedrab;
+}
+
+.sold-out {
+  color: red;
 }
 </style>
